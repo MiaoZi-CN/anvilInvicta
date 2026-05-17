8a08d9f117bcd054cb4554b4f644ae21129b63c4
430230562 <335>
2026-05-17 14:41:22 +0800

    update forceprojector.js

--- scripts/blocks/forceprojector.js
+++ scripts/blocks/forceprojector.js
@@ -38,3 +38,64 @@ directionalForceProjector.buildType = () => extend(DirectionalForceProjector.Dir
  }
 });
 directionalForceProjector.consumePower(10)
+
+let passable = Stat("passable");
+
+const reinforcedForceProjector = extend(ForceProjector, "unit-force-projector", {
+    radius: 72,
+    sides: 4,
+    shieldRotation: 45,
+    shieldHealth: 1200,
+    consumeCoolant: false,
+    hasLiquids: false,
+    cooldownNormal: 40 / 60,
+    cooldownBrokenBase: 50 / 60,
+    phaseRadiusBoost: 0,
+    phaseShieldBoost: 400,
+    phaseUseTime: 600,
+    size: 1,
+    itemConsumer: new ConsumeItems([new ItemStack(Items.silicon, 1)]),
+    //coolantConsumer: new ConsumeLiquid(liquid.naturalGas, 0.1),
+    buildVisibility: BuildVisibility.shown,
+    category: Category.effect,
+    
+    setStats() {
+        this.super$setStats();
+        
+        this.stats.add(passable, false);
+    }
+});
+reinforcedForceProjector.buildType = prov(() => extend(ForceProjector.ForceBuild, reinforcedForceProjector, {
+    updateTile() {
+        this.super$updateTile();
+        
+        let realRadius = this.realRadius();
+        
+        if (realRadius > 0 && !this.broken) {
+            Units.nearbyEnemies(null, this.x, this.y, realRadius, unit => {
+                if (unit.team != this.team && Intersector.isInRegularPolygon(this.block.sides, this.x, this.y, this.realRadius(), this.block.shieldRotation, unit.x, unit.y)) {
+                    if (unit.isMissile()) {
+                        unit.kill()
+                        
+                        this.buildup -= unit.health * 2 * Vars.state.rules.unitDamage(unit.team)
+                        this.block.hitSound.at(unit.x, unit.y, 1 + Mathf.range(0.1), this.block.hitSoundVolume);
+                        this.block.absorbEffect.at(unit);
+                        this.hit = 1
+                    }
+                    //stop
+                    unit.vel.setZero();
+                    //get out
+                    unit.impulse(
+    		            Math.cos(Angles.angle(this.x, this.y, unit.x, unit.y)* Math.PI / 180) * unit.type.hitSize * unit.type.hitSize,
+    					Math.sin(Angles.angle(this.x, this.y, unit.x, unit.y)* Math.PI / 180) * unit.type.hitSize * unit.type.hitSize
+    		        )
+                    
+                    if (Mathf.chanceDelta(0.12 * Time.delta)) {
+                        Fx.circleColorSpark.at(unit.x, unit.y, this.team.color);
+                    }
+                }
+            })
+        }
+    }
+}))
+reinforcedForceProjector.consumePower(2);
